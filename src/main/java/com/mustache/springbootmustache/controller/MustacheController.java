b303b5abcdc58904416e2da7ac6908c40753a502
서정희 <sjhlko@naver.com>
2022-11-07 14:41:23 +0900

    MustacheController 관련 추가

--- src/main/java/com/mustache/springbootmustache/controller/MustacheController.java
+++ src/main/java/com/mustache/springbootmustache/controller/MustacheController.java
@@ -1,5 +1,6 @@
 package com.mustache.springbootmustache.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,18 @@ public class MustacheController {
     @GetMapping("/hi")
     public String mustacheCon(Model model){
         //view에 값을 넘기겠다
-        model.addAttribute("username","heoung");
+        model.addAttribute("username","헝지");
         return "greetings"; //grettings라는 이름의 view를 리턴
 
     }
     @GetMapping("/hi/{id}")
     public String mustacheCon2(@PathVariable String id, Model model){
         //view에 값을 넘기겠다
-        model.addAttribute("username","heoung");
+        model.addAttribute("username","헝지");
         model.addAttribute("id",id);
         return "greetings"; //grettings라는 이름의 view를 리턴
 
     }
+
+
 }
